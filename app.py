c7357aa0cda4d1ed75c41d31ec609d5fd9120a62
Hayesone <joehayesone@gmail.com>
2022-10-31 02:40:43 +0000

    Final Fixes before meeting Monday 31/10/2022
    Changed card layout for subcats to shop styled css.
    Refactored the files and variable names to match more with the changed content names.

--- app.py
+++ app.py
@@ -8,94 +8,94 @@ def landing():
     return render_template("landing.html")
 
 
-@app.route('/shop-services')
+@app.route('/our-services')
 def shop_services():
     return render_template("subcategory/shop-services.html")
 
 
-@app.route('/coaching-development')
+@app.route('/archery-coaching')
 def coaching_development():
     return render_template("subcategory/coaching-dev.html")
 
 
-@app.route('/info')
-def info():
-    return render_template("subcategory/info.html")
+@app.route('/starting-out-archery')
+def starting_out_archery():
+    return render_template("subcategory/starting_out_archery.html")
 
 
-# Contact Us routes to shop contact us
-
-# region Shop & Services content pages
-@app.route('/shop-services/corporate-events')
+# region Our Services content pages
+@app.route('/our-services/corporate-events')
 def ss_corporate():
-    return render_template("subcategory/shop-services/corporate.html")
+    return render_template("subcategory/services/corporate.html")
 
 
-@app.route('/shop-services/parties')
+@app.route('/our-services/parties')
 def ss_parties():
-    return render_template("subcategory/shop-services/parties.html")
+    return render_template("subcategory/services/parties.html")
 
 
-@app.route('/shop-services/compound-bow-check-up')
+@app.route('/our-services/compound-bow-check-up')
 def ss_compound():
-    return render_template("subcategory/shop-services/compound-checkup.html")
+    return render_template("subcategory/services/compound-checkup.html")
 
 
-@app.route('/shop-services/recurve-bow-check-up')
+@app.route('/our-services/recurve-bow-check-up')
 def ss_recurve():
-    return render_template("subcategory/shop-services/recurve-checkup.html")
+    return render_template("subcategory/services/recurve-checkup.html")
 
 
-@app.route('/shop-services/arrow-building')
+@app.route('/our-services/arrow-building')
 def ss_arrow():
-    return render_template("subcategory/shop-services/arrow-building.html")
+    return render_template("subcategory/services/arrow-building.html")
 
 
-@app.route('/shop-services/string-making')
+@app.route('/our-services/string-making')
 def ss_string():
-    return render_template("subcategory/shop-services/string-making.html")
+    return render_template("subcategory/services/string-making.html")
 
 
 # endregion
 
+# Shop routes straight to the shop
+
 # region Coaching & Development
 
-@app.route('/coaching-development/one-to-one-coaching')
+@app.route('/archery-coaching/one-to-one-coaching')
 def coaching_1to1():
-    return render_template("subcategory/coaching-dev/1-1-coaching.html")
+    return render_template("subcategory/archery-coaching/1-1-coaching.html")
 
 
-@app.route('/coaching-development/group-coaching-courses')
+@app.route('/archery-coaching/group-coaching-courses')
 def coaching_group():
-    return render_template("subcategory/coaching-dev/group-coaching.html")
+    return render_template("subcategory/archery-coaching/group-coaching.html")
 
 
-@app.route('/coaching-development/third-eye-archery-training')
+@app.route('/archery-coaching/third-eye-archery-training')
 def coaching_third():
-    return render_template("subcategory/coaching-dev/third-eye.html")
+    return render_template("subcategory/archery-coaching/third-eye.html")
 
 
 # endregion
 
 # region Info on Archery & Products
 
-@app.route('/info/have-a-go-sessions')
-def info_have_a_go():
+@app.route('/starting-out-archery/have-a-go-sessions')
+def starting_out_have_a_go():
     return render_template("subcategory/info/have-a-go.html")
 
 
-@app.route('/info/beginner-courses')
-def info_beginners():
+@app.route('/starting-out-archery/beginner-courses')
+def starting_out_beginners():
     return render_template("subcategory/info/beginners-courses.html")
 
 
-@app.route('/info/your-first-kit')
-def info_your_first_kit():
+@app.route('/starting-out-archery/your-first-kit')
+def starting_out_your_first_kit():
     return render_template("subcategory/info/your-first-kit.html")
 
 
-@app.route('/info/local-clubs')
-def info_local_clubs():
+@app.route('/starting-out-archery/local-clubs')
+def starting_out_local_clubs():
     return render_template("subcategory/info/local-clubs.html")
 
 # No route for kent archery as direct link out
